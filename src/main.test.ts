8f8eb27a7ddfb57b12d5f31b90f1e04989f75067
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2024-10-07 14:04:07 +0900

    環境構築

---
+++ src/main.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('下端点と上端点を受け取って、プロパティstartに3とプロパティendに8を持ったインスタンスが作成されること', () => {
+    expect(true).toBe(true)
+})
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+export class Range {
+  start: number;
+  end: number;
+  
+  constructor(start: number, end: number) {
+    this.start = start;
+    this.end = end;
+  }
+
+}

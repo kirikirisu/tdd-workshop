27e8e40c58aa0820214ee2e46e4b8eb59aeb0b75
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2024-10-07 14:23:38 +0900

    refeactor: Rangeクラスを作成できること

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,19 +1,22 @@
 import { describe, expect, test } from 'vitest'
 import {Range} from './main'
 
+describe('Rangeクラスを作成できること', () => {
+    test('下端点: 3 と上端点: 8 を受け取って、プロパティstartに3とプロパティendに8を持ったRangeインスタンスが作成されること', () => {
+        const inputStart = 3, inputEnd = 8;
 
-test('下端点: 3 と上端点: 8 を受け取って、プロパティstartに3とプロパティendに8を持ったRangeインスタンスが作成されること', () => {
-    const inputStart = 3, inputEnd = 8;
+        const actualRange = new Range(inputStart, inputEnd);
 
-    const actualRange = new Range(inputStart, inputEnd);
-
-    /* 下端点と上端点は密接に紐づいているため同時にassertする */
-    expect(actualRange.start).toBe(inputStart)
-    expect(actualRange.end).toBe(inputEnd)
+        /* 下端点と上端点は密接に紐づいているため同時にassertする */
+        expect(actualRange.start).toBe(inputStart)
+        expect(actualRange.end).toBe(inputEnd)
+    })
 })
+
 
 describe("上端点より下端点が大きい閉区間を作ることができないこと", () => {
     test("上端点より下端点の方が大きい値を受け取った時エラーを投げること", () => {
        expect(() => new Range(7, 3)).toThrowError("上端点より下端点の方が大きい閉区間は作れません")
     })
 })
+

7a2c038d978ad30f8b500acb7ae232c8ced324c6
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2024-10-07 14:11:40 +0900

    test: Rangeインスタンスを作れること

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from 'vitest'
+import {Range} from './main'
 
-test('下端点と上端点を受け取って、プロパティstartに3とプロパティendに8を持ったインスタンスが作成されること', () => {
-    expect(true).toBe(true)
+test('下端点: 3 と上端点: 8 を受け取って、プロパティstartに3とプロパティendに8を持ったRangeインスタンスが作成されること', () => {
+    const inputStart = 3, inputEnd = 8;
+
+    const actualRange = new Range(inputStart, inputEnd);
+
+    /* 下端点と上端点は密接に紐づいているため同時にassertする */
+    expect(actualRange.start).toBe(inputStart)
+    expect(actualRange.end).toBe(inputEnd)
 })

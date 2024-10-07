a896ba659eaf0b2cec7ecb50a19c0c8bbcdeead4
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2024-10-07 14:20:47 +0900

    imple: 上端点より下端点の方が大きい値を受け取った時niエラーを投げる

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,6 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import {Range} from './main'
+
 
 test('下端点: 3 と上端点: 8 を受け取って、プロパティstartに3とプロパティendに8を持ったRangeインスタンスが作成されること', () => {
     const inputStart = 3, inputEnd = 8;
@@ -11,7 +12,7 @@ test('下端点: 3 と上端点: 8 を受け取って、プロパティstartに3
     expect(actualRange.end).toBe(inputEnd)
 })
 
-test("上端点より下端点が大きい閉区間を作ることができないこと", () => {
+describe("上端点より下端点が大きい閉区間を作ることができないこと", () => {
     test("上端点より下端点の方が大きい値を受け取った時エラーを投げること", () => {
        expect(() => new Range(7, 3)).toThrowError("上端点より下端点の方が大きい閉区間は作れません")
     })
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ export class Range {
   end: number;
   
   constructor(start: number, end: number) {
+    if (start > end) throw new Error('上端点より下端点の方が大きい閉区間は作れません');
+
     this.start = start;
     this.end = end;
   }
+
 
 }
